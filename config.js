9393d849507351eb5b9cb80118f79f36034627bb
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-08 20:57:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN || '7646559842:AAGYPtAjuIWSciqJoh1-QpuqaoHwvI0xFK0',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://kurachmantopik:kennbot@cluster0.fprfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  ADMIN_IDS: (process.env.ADMIN_IDS || '').split(',').map(id => Number(id)),
+  ADMIN_IDS: process.env.ADMIN_IDS
+    ? process.env.ADMIN_IDS.split(',').map(id => Number(id.trim())).filter(id => id > 0)
+    : [],
 };
+
+console.log("Admin IDs:", module.exports.ADMIN_IDS);
