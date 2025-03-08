86362c9de5ccb7bcb3ccbca9be99d5b5bac832ab
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-03-08 20:45:21 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN || '7646559842:AAGYPtAjuIWSciqJoh1-QpuqaoHwvI0xFK0',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://kurachmantopik:kennbot@cluster0.fprfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  ADMIN_IDS: (process.env.ADMIN_IDS || '').split(',').map(id => Number(id)),
+};
